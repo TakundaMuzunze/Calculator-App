3f27f51ea366932a7747476407074cd9a053e480
Takunda Muzunze <takmuzunze@gmail.com>
2023-06-11 17:15:49 +0100

    Update logic.js
    added function for calculator app to perform arithmetic operations

--- logic.js
+++ logic.js
@@ -1,16 +1,17 @@
 const defaultDisplay = document.getElementById('screen');
 let isDefaultDisplayed = false;
+let resultDisplay = '';
 
-function defaultScreen() {
+function defaultScreen() { //Default Display Function
     if (!isDefaultDisplayed) {
       defaultDisplay.value = '0';
       isDefaultDisplayed = true;
     }
 }
 
-defaultScreen();
+defaultScreen(); 
 
-function buttonClick(button) {
+function buttonClick(button) { // Display value when clicked
     if (defaultDisplay.value === '0') {
       defaultDisplay.value = ''; 
     }
@@ -18,7 +19,14 @@ function buttonClick(button) {
     button.blur();
 }
 
-function clearDisplay (button) {
+function sumOfValue (){ //Display result of 2 or more values. CODE HERE
+    const expression = defaultDisplay.value;
+    const sumResult = eval(expression);
+
+    defaultDisplay.value = sumResult;
+}
+
+function clearDisplay (button) { //Clear display input when clear button is clicked
     if (defaultDisplay !== '0'){
         defaultDisplay.value = '0';
     }
